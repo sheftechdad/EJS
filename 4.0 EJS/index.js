36f96abc012c359f06c0dfb393a5a1d8665e286e
sheftechdad <shefalichaurasia30945@gmail.com>
2024-09-28 15:10:07 +0530

    ejs day one

---
+++ 4.0 EJS/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+
+const app=express();
+const port=3000;
+
+app.get("/" ,(req, res)=>{
+    const d=new Date();
+    const day=d.getDay();
+
+    //console.log(day);
+
+
+    let type="a weekday";
+    let adv="it's time to work hard";
+
+    if (day === 0 || day === 6){
+        type="a weekend";
+        adv="it's time to  have fun";
+        
+
+
+    }
+
+    res.render("index.ejs" ,{
+        dayType:type,
+        advice:adv,
+
+
+
+    })
+
+
+}
+);
+app.listen(port,()=>{
+    console.log(`listening port no ${port}`);
+
+});
